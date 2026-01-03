ea38d4092b27f07a86fd3879cdb4b2e00790c900
denzquix <denzquix@gmail.com>
2026-01-03 15:57:36 +0000

    Adjust Pro lib names and version codes

--- src/registry.ts
+++ src/registry.ts
@@ -7,11 +7,11 @@ export interface LibInfo {
 
 const registry: {[md5: string]: LibInfo} = {
   // official
-  'ab1d615bde71d06d53de098768e5384e': {name:'Core', version:'ProV2', libraryNumber:0},
-  'ddf2f65aaa9df85d642317e199105d53': {name:'Music', version:'ProV2', libraryNumber:1},
-  '57e035efd4d37cb5504bd7d619a99933': {name:'Compact', version:'ProV2', libraryNumber:2},
-  '7d3df35f6a37886c2d4c84542b9300e0': {name:'Requester', version:'ProV2', libraryNumber:3},
-  '241e45afbbad3871e5dad9135dacd1cb': {name:'IOPorts', version:'ProV2', libraryNumber:6},
+  'ab1d615bde71d06d53de098768e5384e': {name:'ProCore', version:'V2.00', libraryNumber:0},
+  'ddf2f65aaa9df85d642317e199105d53': {name:'ProMusic', version:'V2.00', libraryNumber:1},
+  '57e035efd4d37cb5504bd7d619a99933': {name:'ProCompact', version:'V2.00', libraryNumber:2},
+  '7d3df35f6a37886c2d4c84542b9300e0': {name:'ProRequester', version:'V2.00', libraryNumber:3},
+  '241e45afbbad3871e5dad9135dacd1cb': {name:'ProIOPorts', version:'V2.00', libraryNumber:6},
 
   '1287e5ce2930a37b23c892973f225749': {name:'CreatorCore', version:'V1.12', libraryNumber:0},
   '2d4087f5635f1617695f2119c8a6b8e3': {name:'CreatorCore', version:'V1.34', libraryNumber:0},
